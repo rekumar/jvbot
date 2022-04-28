9529334cd82575a89307068f57a27b818cc2c83f
Deniz93 <49666626+Deniz93@users.noreply.github.com>
2022-04-28 02:18:15 -0700

    gui now runs, time to code

--- ui/RUN_UI.py
+++ ui/RUN_UI.py
@@ -11,8 +11,10 @@ import time
 # copying over from PARASOL
 # Set module directory, load yaml preferences
 MODULE_DIR = os.path.dirname(__file__)
-with open(os.path.join(MODULE_DIR, "..", "hardwareconstants.yaml"), "r") as f:
-    defaults = yaml.safe_load(f)["RUN_UI"]  # , Loader=yaml.FullLoader)["relay"]
+TRAY_VERSIONS_DIR = os.path.join(MODULE_DIR, "tray_versions")
+
+# with open(os.path.join(TRAY_VERSIONS_DIR,  "storage_v1.yaml"), "r") as f:
+#     defaults = yaml.safe_load(f)["RUN_UI"]  # , Loader=yaml.FullLoader)["relay"]
 
 # Ensure resolution/dpi is correct for UI
 if hasattr(QtCore.Qt, "AA_EnableHighDpiScaling"):
@@ -33,29 +35,28 @@ def RUNNER():
             super(RUN_UI, self).__init__()
 
             # Initialize packages
-            self.controller = Controller()
-            self.characterization = Characterization()
-            self.analysis = Analysis()
-            self.grapher = Grapher()
-            self.filestructure = FileStructure()
+            # self.controller = Controller()
+            # self.characterization = Characterization()
+            # self.analysis = Analysis()
+            # self.grapher = Grapher()
+            # self.filestructure = FileStructure()
 
             # Make blank variables for the start date
             self.startdate1 = None
-            self.startdate2 = None
-            self.startdate3 = None
-            self.startdate4 = None
-            self.startdate5 = None
-            self.startdate6 = None
-
+ 
             # Make blank variables for saveloc
             self.savedir1 = None
-            self.savedir2 = None
-            self.savedir3 = None
-            self.savedir4 = None
-            self.savedir5 = None
-            self.savedir6 = None
 
             # Load the ui file
             ui_path = os.path.join(MODULE_DIR, "RUN_UI.ui")
             uic.loadUi(ui_path, self)
             self.show()
+            
+        # Create application (required for widget)
+    app = QApplication(sys.argv)
+
+    # Create UI, which is a widget
+    window = RUN_UI()
+
+    # Start the application
+    app.exec_()
